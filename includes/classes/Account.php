0247d2a2b9b6d8d8b8f12cfb38c86d27dc7b89e1
MiranAmin <miranamin@hotmail.co.uk>
2021-03-04 16:55:20 +0000

    Lots of changes

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,5 +1,4 @@
 <?php
-
 class Account {
 
     private $con;
@@ -9,34 +8,29 @@ class Account {
         $this->con = $con;
     }
 
-
     public function register($fn, $ln, $un, $em, $em2, $pw, $pw2) {
         $this->validateFirstName($fn);
         $this->validateLastName($ln);
         $this->validateUsername($un);
-        $this->validateEmail($em, $em2);
-
-
-        
+        $this->validateEmails($em, $em2);
+        $this->validatePasswords($pw, $pw2);
     }
 
-
     private function validateFirstName($fn) {
-        if(strlen($fn) < 2 || strlen($fn) > 25 ) {
-            array_push($this->errorArray, Constants::$firstNameError);
+        if(strlen($fn) < 2 || strlen($fn) > 25) {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
         }
     }
 
     private function validateLastName($ln) {
-        if(strlen($ln) < 2 || strlen($ln) > 25 ) {
-            array_push($this->errorArray, Constants::$lastNameError);
+        if(strlen($ln) < 2 || strlen($ln) > 25) {
+            array_push($this->errorArray, Constants::$lastNameCharacters);
         }
     }
 
-    //Can breakout the query area of this function into another for reuse. its repeated code
     private function validateUsername($un) {
-        if(strlen($un) < 2 || strlen($un) > 25 ) {
-            array_push($this->errorArray, Constants::$usernameError);
+        if(strlen($un) < 2 || strlen($un) > 25) {
+            array_push($this->errorArray, Constants::$usernameCharacters);
             return;
         }
 
@@ -44,41 +38,49 @@ class Account {
         $query->bindValue(":un", $un);
 
         $query->execute();
-
+        
         if($query->rowCount() != 0) {
             array_push($this->errorArray, Constants::$usernameTaken);
         }
-
     }
 
-    //Can breakout the query area of this function into another for reuse. its repeated code
-    public function validateEmail($em, $em2) {
-        if($em !== $em2) {
+    private function validateEmails($em, $em2) {
+        if($em != $em2) {
             array_push($this->errorArray, Constants::$emailsDontMatch);
+            return;
         }
 
         if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
             array_push($this->errorArray, Constants::$emailInvalid);
             return;
         }
-        
+
         $query = $this->con->prepare("SELECT * FROM users WHERE email=:em");
         $query->bindValue(":em", $em);
 
         $query->execute();
-
+        
         if($query->rowCount() != 0) {
             array_push($this->errorArray, Constants::$emailTaken);
         }
-        
+    }
+
+    private function validatePasswords($pw, $pw2) {
+        if($pw != $pw2) {
+            array_push($this->errorArray, Constants::$passwordsDontMatch);
+            return;
+        }
+
+        if(strlen($pw) < 5 || strlen($pw) > 25) {
+            array_push($this->errorArray, Constants::$passwordLength);
+        }
     }
 
     public function getError($error) {
         if(in_array($error, $this->errorArray)) {
-            return $error;
+            return "<span class='errorMessage'>$error</span>";
         }
     }
-
 
 }
 ?>
--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,16 +1,13 @@
-<?php 
+<?php
 class Constants {
- 
-    public static $firstNameError = "Your first name must be between 2 and 25 characters!";
-    public static $lastNameError = "Your last name must be between 2 and 25 characters!";
-    public static $usernameError = "Your username must be between 2 and 25 characters!";
-    public static $usernameTaken = "Username is already taken!";
-    public static $emailsDontMatch = "Both emails must match!";
+    public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+    public static $usernameCharacters = "Your username must be between 2 and 25 characters";
+    public static $usernameTaken = "Username already in use";
+    public static $emailsDontMatch = "Your emails don't match";
     public static $emailInvalid = "Invalid email";
-    public static $emailTaken = "This email is already taken";
-
+    public static $emailTaken = "Email already in use";
+    public static $passwordsDontMatch = "Passwords don't match";
+    public static $passwordLength = "Your password must be between 5 and 25 characters";
 }
-
-
-
 ?>
