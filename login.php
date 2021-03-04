3b317e467f73b5720d236457ae6700988710a0be
MiranAmin <miranamin@hotmail.co.uk>
2021-03-04 17:54:11 +0000

    Register and login are done. Should refactor for better OOP.

--- login.php
+++ login.php
@@ -1,7 +1,28 @@
 <?php
+    require_once("includes/config.php");
+    require_once("includes/classes/FormSanitizer.php");
+    require_once("includes/classes/Constants.php");
+    require_once("includes/classes/Account.php");
+    
+    $account = new Account($con);
 
-    if(isset($_POST["submitButton"])) {
+     if(isset($_POST["submitButton"])) {
+        
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        
+        $success = $account->login($username, $password);
+        
+        if($success) {
+            $_SESSION['userLoggedIn'] = $username;
+            header("Location: index.php"); //just means go to this page
+        }
+    }
 
+    function getInputValue($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
     }
 
 ?>
@@ -32,7 +53,8 @@
 
             <form method="post" action="">
 
-                <input type="text" name="username" placeholder="Username" required>
+                <?php echo $account->getError(Constants::$loginFailed); ?>
+                <input type="text" name="username" placeholder="Username" value="<?php getInputValue("username");?>"required>
                 
                 <input type="password" name="password" placeholder="Password" required>
             
